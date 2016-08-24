d466f76491374bffc9363e39900e11010596ff7c
Hirewand <shashank@hirewand.com>
2016-08-24 13:02:57 +0530

    Add files via upload

---
+++ src/org/hirewand/HWHTTPException.java
@@ -0,0 +1,29 @@
+package org.hirewand;
+
+public class HWHTTPException extends Exception {
+	String message = null;
+	int code = 0;
+	public HWHTTPException(String message, int code) {
+        super(message);
+        this.message = message;
+        this.code = code;
+    }
+	
+	public HWHTTPException(Throwable cause) {
+        super(cause);
+    }
+	
+	@Override
+    public String toString() {
+        return message;
+    }
+ 
+    @Override
+    public String getMessage() {
+        return message;
+    }
+    
+    public int getCode() {
+        return code;
+    }
+}
---
+++ src/org/hirewand/InvalidRequestException.java
@@ -0,0 +1,29 @@
+package org.hirewand;
+
+public class InvalidRequestException extends Exception{
+	String message = null;
+	int code = 0;
+	public InvalidRequestException(String message, int code) {
+        super(message);
+        this.message = message;
+        this.code = code;
+    }
+	
+	public InvalidRequestException(Throwable cause) {
+        super(cause);
+    }
+	
+	@Override
+    public String toString() {
+        return message;
+    }
+ 
+    @Override
+    public String getMessage() {
+        return message;
+    }
+    
+    public int getCode() {
+        return code;
+    }
+}
